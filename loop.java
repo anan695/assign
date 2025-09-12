99fcf8e2c7a5b1aa573844bfef9c2bb62f02f8e4
anan695 <pandeyananyajyoti@gmail.com>
2025-09-12 13:43:33 +0530

    update loop.java

---
+++ loop.java
@@ -0,0 +1,161 @@
+// public class loop {
+//     public static void main(String[] args) {
+        // for(int counter=0; counter<3;counter=counter+1){
+        //     System.out.println("Hello World");
+        // }
+
+        // for(int counter=0; counter<11;counter++){
+        //     System.out.println(counter);
+        // }
+
+
+        // int i=0;
+        // while(i<11){
+        // System.out.println(i);
+        // i=i+1;
+        // }
+
+
+
+//         int i=0;
+//         do{
+//             System.out.println(i);
+//             i=i+1;
+//         }
+//         while(i<11);
+//     }
+// }
+
+
+
+
+//print the sum of n natural numbers
+// import java.util.*;
+// public class loop {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int sum=0;
+//         for(int i=1;i<n;i++){
+//             sum=sum+i;
+
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+
+//print the table of a number input by the user
+// import java.util.*;
+// public class loop {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=1;i<11;i++){
+//             System.out.println(i*n);
+
+//         }
+//     }
+// }
+
+
+
+//print all even numbers till n
+// import java.util.*;
+// public class loop {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=0;i<n;i=i+2){
+//             System.out.println(i);
+//         }
+
+
+//    for(;  ;){
+//     System.out.println("Apna College");
+//    }
+//     }
+// }
+
+
+
+// import java.util.*;
+// public class loop {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int input;
+//         do{
+//             int marks=sc.nextInt();
+//             if(marks>=90&&marks<=100){
+//                 System.out.println("This is Good");
+//             }
+//             else if(marks>=60&&marks<=89){
+//                 System.out.println("This is also Good");
+//             }
+//             else if(marks>=0&&marks<=59){
+//                 System.out.println("This is Good as well");
+//             }
+//             else{
+//                 System.out.println("Invalid");
+//             }
+//             System.out.println("Want to continue ? (yes(1) or no(0))");
+//             input=sc.nextInt();
+//         }
+//         while(input==1);
+//     }
+// }
+
+
+
+//print the prime number and take the imput n from the user
+// import java.util.*;
+// public class loop {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         boolean isPrime=true;
+//         for(int i=2;i<=n/2;i++){
+//             if(n%i==0){
+//                 isPrime=false;
+//                 break;
+//             }
+//         }
+//         if(isPrime){
+//             if(n==1){
+//                 System.out.println("This is neither prime nor composite");
+//             }
+//             else {
+//                 System.out.println("This is a prime number");
+//             }
+//             else{
+//                 System.out.println("This is not a prime number");
+//             }
+//         }
+//     }
+// }
+
+//Using a while loop, find the factorial of a given number.
+// import java.util.*;
+// public class loop {
+
+//         public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int fact=0;
+//         while(i<=n){
+
+//         }
+                
+//         }
+// }
+
+
+
+
+
+
