75110be48b85e7af8f32185addcc739897dfb136
anan695 <pandeyananyajyoti@gmail.com>
2025-09-12 13:45:04 +0530

    update exe.java

---
+++ exe.java
@@ -0,0 +1,187 @@
+//Enter 3 numbers from the user & make a function to print their average.
+
+
+// import java.util.*;
+
+// public class exe {
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         int c=sc.nextInt();
+//         int average=(a+b+c)/3;
+//         System.out.println(average);
+
+        
+//     }
+    
+// }
+
+//Write a function to print the sum of all odd numbers from 1 to n.
+// import java.util.*;
+// public class exe {
+//     public static void printsum(int n){
+//         int sum=0;
+//         for(int i=1;i<=n;i++){
+//             if(i%2!=0){
+//                 sum=sum+i;
+//             }
+//         }
+//         System.out.println(sum);
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+
+//         printsum(n);
+
+//     }
+// }
+
+//Write a function which takes in 2 numbers and returns the greater of those two.
+// import java.util.*;
+// public class exe {
+//     public static  int getGreater(int a,int b) {
+//         if(a>b){
+//             return a;
+//         }
+//         else{
+//             return b;
+//         }
+        
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         System.out.println(getGreater(a, b));
+
+//     }
+// }
+
+
+//Write a function that takes in the radius as input and returns the circumference of a circle.
+// import java.util.*;
+
+// public class exe {
+//     public static Double getCircumference(Double r){
+//         return 2*3.14*r;
+
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         Double r=sc.nextDouble();
+//         System.out.println(getCircumference(r));
+//     }
+// }
+
+//Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+// import java.util.*;
+// public class exe {
+//     public static boolean iseligible(int age){
+//         if(age>18){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int age=sc.nextInt();
+//         System.out.println(iseligible(age));
+//     }
+// }
+
+//write an infinite loop using do while condition
+// import java.util.*;
+// public class exe {
+    
+
+//     public static void main(String[] args) {
+//         do{
+
+//         } while(true);
+
+        
+//     }
+// }
+
+
+// write a program to enter the numbers till the users want and at the end it should display the count of positive,negative and zeros entered
+// import java.util.*;
+// public class exe {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Press 1 to continue and 0 to stop");
+//         int i =sc.nextInt();
+//         int n=sc.nextInt();
+//         while(i==1){
+//             if(n>0){
+//                 System.out.println("positive");;
+//             }
+//             else if(n<0){
+//                 System.out.println("negative");
+//             }
+//             else{
+//                 System.out.println("Zeores");;
+//             }
+//             System.out.println("Press 1 to continue and 0 to stop");
+//              int input =sc.nextInt();
+
+//         }
+//         System.out.println("Positives:");
+//         System.out.println("Negative");
+//         System.out.println("Zeores");
+//     }
+// }
+
+
+//two numbers are entered by the usre x and n.write a function to find the value of one number raised to the power of another i.e.x^n
+// import java.util.*;
+// public class exe {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int x=sc.nextInt();
+//         int n=sc.nextInt();
+//         int result =1;
+//         for(int i=0;i<n;i++){
+
+//         }
+//         System.out.println(result);
+//     }
+// }
+
+// write a function that calculates the Greatest common Divisior of 2 numbers
+// import java.util.*;
+// public class exe {
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in){
+//             int n1=sc.nextInt();
+//             int n2=sc.nextInt();
+//             while(n1!=n2){
+//                 if(n1>n2){
+//                     n1=n1-n2;
+//                 }
+//                 else{
+//                     n2=n2-n1;
+//                 }
+//         System.out.println("GCD"+n2);
+
+
+//             }
+            
+//         }
+//     }
+// }
+
+
+
+
+
