a7113412991bc1204f84be758fa04811db07770e
anan695 <pandeyananyajyoti@gmail.com>
2025-09-12 13:44:14 +0530

    update sting.java

---
+++ sting.java
@@ -0,0 +1,42 @@
+//Write a program to find and return the length of a string without using the length() method     
+// import java.util.*;
+// public class sting {
+    
+    
+//     public static int getLengthWithoutUsingLength(String str) {
+//         int count = 0;
+//         try {
+//             while (true) {
+//                 str.charAt(count); 
+//                 count++;
+//             }
+//         } catch (StringIndexOutOfBoundsException e) {
+        
+//         }
+//         return count;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+        
+//         System.out.print("Enter a string: ");
+//         String input = scanner.next();
+
+        
+//         int customLength = getLengthWithoutUsingLength(input);
+
+        
+//         int builtInLength = input.length();
+
+        
+//         System.out.println("Length using custom method: " + customLength);
+//         System.out.println("Length using built-in method: " + builtInLength);
+
+//         scanner.close();
+//     }
+// }
+
+
+    
+
